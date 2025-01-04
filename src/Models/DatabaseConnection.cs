88784723918da0ee5124fdd5027e87f44ba4befa
kenzo-b <contact@kbriche.fr>
2025-01-04 15:45:32 +0100

    change db info and remove unused function

--- src/Models/DatabaseConnection.cs
+++ src/Models/DatabaseConnection.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
+﻿using MySql.Data.MySqlClient;
 
 namespace Menagelec.src.Models
 {
@@ -13,8 +7,8 @@ namespace Menagelec.src.Models
         private const string SERVER = "localhost";
         private const string PORT = "3306";
         private const string DATABASE = "menageleccsharp";
-        private const string UID = "menagelec";
-        private const string PASSWORD = "MenagelecSIO2024!";
+        private const string UID = "root";
+        private const string PASSWORD = "";
 
 
         public static MySqlConnection connect()
@@ -28,25 +22,6 @@ namespace Menagelec.src.Models
             MySqlConnection connect = new MySqlConnection(connectInfo);
             connect.Open();
             return connect;
-        }
-
-        public MySqlCommand mapParameters(MySqlCommand cmd)
-        {
-            string modelClassName = this.GetType().ToString();
-            string className = modelClassName.Replace("Model", "");
-            Type objType = Type.GetType(className);
-
-            object instance = Activator.CreateInstance(objType);
-            var getParametersMethod = objType.GetMethod("getParameters");
-            var parameters = getParametersMethod.Invoke(instance, null) as Dictionary<string, string>;
-
-            foreach(KeyValuePair<string, string> param in parameters)
-            {
-                cmd.Parameters.AddWithValue(param.Key, param.Value);
-            }
-
-            return cmd;
-            
         }
     }
 }
